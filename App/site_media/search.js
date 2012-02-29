b7204b7bfa62d684fc59996b2098f50c611fa78b
Henry S <henrythe9th@gmail.com>
2012-02-28 22:22:52 -0500

    Changed search to show rounded icons

--- App/site_media/search.js
+++ App/site_media/search.js
@@ -1,6 +1,8 @@
 function search_submit() {
 	var query = $("#id_query").val();
 	$("#search_results").load(/search/?ajax&query="+ encodeURIComponent(query));
+	$("#search_results").hide();
+	$("#search_results").fadeIn();
 	return false;
 }
 
